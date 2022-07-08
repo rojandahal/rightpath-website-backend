2763e447528d1ff17b5678128c81c8e1b383251a
rojandahal <maxpanye60@gmail.com>
2022-07-08 22:31:22 +0545

    Cors Updated

--- server.js
+++ server.js
@@ -14,13 +14,12 @@ const app = express()
 //Connect to database
 //Body parser
 // Use of cors
-app.use(
-    cors({
-      origin: "http://localhost:3001",
-      methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "DELETE"],
-      credentials: true,
-    })
-  );
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
   app.use(express.json());
   
   // Cookies parser
