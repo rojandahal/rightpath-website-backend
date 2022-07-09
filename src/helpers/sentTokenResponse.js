94ed6504a465bbf0d8c26cdcb56a0ff3a852ebcc
sammyview80 <52382079+sammyview80@users.noreply.github.com>
2022-07-09 22:36:52 +0545

    refator: don't set cookies

--- src/helpers/sentTokenResponse.js
+++ src/helpers/sentTokenResponse.js
@@ -1,22 +1,31 @@
-const { sendResponse } = require("./response");
+const { sendResponse } = require('./response')
 
 const sendTokenResponse = (user, statusCode, res) => {
-    const token = user.getSignedJwtToken();
+  const token = user.getSignedJwtToken()
 
-    let options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 10000),
-        httpOnly: true
-    }
+  let options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 10000
+    ),
+    httpOnly: true
+  }
 
-    if(process.env.NODE_ENV === 'production'){
-        options.secure = true
-    }
+  if (process.env.NODE_ENV === 'production') {
+    options.secure = true
+  }
 
-    return sendResponse(res, {
-        status: "Sucess",
-        token
-    }, statusCode, 'application/json', setCookie=true, options=options)
+  return sendResponse(
+    res,
+    {
+      status: 'Sucess',
+      token,
+      user
+    },
+    statusCode,
+    'application/json',
+    (setCookie = false),
+    (options = options)
+  )
 }
 
-
-module.exports = sendTokenResponse;
+module.exports = sendTokenResponse
